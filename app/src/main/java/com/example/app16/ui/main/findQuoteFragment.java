607c60416279c337fc548faa8f56eefb922a32fd
Lok Yin (william) Chin <lok.chin@kcl.ac.uk>
2021-03-25 19:27:43 +0000

    reslove conflict
    reslove conflict

--- app/src/main/java/com/example/app16/ui/main/findQuoteFragment.java
+++ app/src/main/java/com/example/app16/ui/main/findQuoteFragment.java
@@ -1,39 +1,22 @@
 package com.example.app16.ui.main;
 
 
-import androidx.appcompat.app.AppCompatActivity;
-
-
 import android.os.Bundle;
-import android.content.res.Resources;
-import android.graphics.drawable.Drawable;
-import androidx.core.content.res.ResourcesCompat;
-import android.content.res.AssetManager;
-import android.graphics.drawable.BitmapDrawable;
-import java.io.InputStream;
 
 import android.view.LayoutInflater;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.view.inputmethod.InputMethodManager;
-import androidx.annotation.Nullable;
+
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import com.example.app16.R;
 import android.content.Context;
-import androidx.annotation.LayoutRes;
-import androidx.fragment.app.FragmentPagerAdapter;
-import androidx.viewpager.widget.ViewPager;
-import androidx.fragment.app.FragmentManager;
 import android.view.View.OnClickListener;
-import java.util.List;
-import java.util.ArrayList;
 import android.view.View;
 import android.util.Log;
 import android.widget.Toast;
-import android.widget.RadioGroup;
 import android.widget.EditText;
-import android.webkit.WebView;
 import android.widget.TextView;
 
 
@@ -68,14 +51,16 @@ public class findQuoteFragment extends Fragment implements OnClickListener
   public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
   { root = inflater.inflate(R.layout.findquote_layout, container, false);
     Bundle data = getArguments();
-    findQuotedateTextField = (EditText) root.findViewById(R.id.findQuotedateField);
+    findQuotedateTextField = (EditText) root.findViewById(R.id.editTextDate2);
+      findQuotedateTextField.setOnClickListener(this);
     findQuoteResult = (TextView) root.findViewById(R.id.findQuoteResult);
     findquotebean = new findQuoteBean(myContext);
     findQuoteOkButton = root.findViewById(R.id.findQuoteOK);
     findQuoteOkButton.setOnClickListener(this);
     findQuotecancelButton = root.findViewById(R.id.findQuoteCancel);
     findQuotecancelButton.setOnClickListener(this);
-    return root;
+
+      return root;
   }
 
 
